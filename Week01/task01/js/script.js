5038589daacf069e9443ae5f265956b45539bd22
olivax28 <olivax28@outlook.com>
2025-01-16 14:54:53 -0500

    task 01 done

---
+++ Week01/task01/js/script.js
@@ -0,0 +1,49 @@
+/**
+ * task 01
+ * Olivia Axiuk
+ * 
+ * descriptions
+ */
+
+"use strict";
+
+
+let x= 100;
+let y = 5;
+let width = 100;
+let r = 255;
+let g = 0;
+let b = 0;
+
+
+
+
+
+
+/**
+* setup
+*/
+function setup() {
+createCanvas (720,720)
+
+}
+
+
+/**
+* draw
+*/
+function draw() {
+background(0,0,0)
+push();
+fill(r, g, b);
+ellipse(x, y , width);
+pop();
+push();
+fill(r - 255, g + 255, b);
+ellipse(x +100, y + 500 ,width + 50);
+pop();
+push();
+fill(r-255, g, b+255);
+ellipse(x +600, y + 500 ,width + 10);
+pop();
+}
