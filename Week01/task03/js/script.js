37ab09e2398c1c08c30e60a43c6156e177f1dde0
olivax28 <olivax28@outlook.com>
2025-01-22 23:23:18 -0500

    almost done exercise03

--- Week01/task03/js/script.js
+++ Week01/task03/js/script.js
@@ -15,20 +15,26 @@ let rect02x = 200;
 let rect02y = 200;
 
 
+let rect01x = 200;
+let rect01y = 400;
+
+
 let width = 50;
 let height = 20;
 
 
 let r = 255;
 let g = 174;
-let b = 231; 
+let b = 231;
+
+let reset = 0;
 /**
  * setup
 */
 
 
 function setup() {
-createCanvas (720,720);
+    createCanvas(720, 720);
 }
 
 
@@ -36,24 +42,42 @@ createCanvas (720,720);
  * draw
 */
 function draw() {
-    background (0,0,0);
-    drawRectangles(rect03x,rect03y);
-    drawRectangles(rect02x,rect02y);
-    drawRectangles(200,400);
-
+    background(0, 0, 0);
+    drawRectangles(rect03x, rect03y);
+    drawRectangles(rect02x, rect02y);
+    drawRectangles(rect01x, rect01y);
     moveRectangle03();
 }
 
-function drawRectangles(x,y){
+function drawRectangles(x, y) {
     push();
-    fill(r,g,b);
-    rect(x,y,width,height);
+    fill(r, g, b);
+    rect(x, y, width, height);
     pop();
 }
 
-function moveRectangle03(){
-rect03y = rect03y + 1;
+function moveRectangle03() {
+    rect03y = rect03y + 1;
+    if (rect03y < 720) {
+        resetY
+        console.log(rect03y)
+    }
+}
 
+function mousePressed() {
+    rect02x += 5;
+    rect02y += 5;
+}
+
+function keyPressed() {
+    if (keyCode === 32) {
+        rect01x += 5;
+        rect01y += 5;
+    }
+}
+
+function resetY() {
+    rect03y = 200;
 }
 
 
