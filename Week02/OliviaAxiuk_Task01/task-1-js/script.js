249abf757b48ccf585e043216bf128d68728d490
olivax28 <olivax28@outlook.com>
2025-01-29 16:30:12 -0500

    started question 03 part 02

--- Week02/OliviaAxiuk_Task01/task-1-js/script.js
+++ Week02/OliviaAxiuk_Task01/task-1-js/script.js
@@ -212,33 +212,38 @@ length
     /*************************************** */
     /* 1: NEW PARAGRAPHS */
     /* 1A: Access all paragraph elements, and store the result in a variable called: allPTagsThree */
-    let allPTagsThree = document.querySelectorAll("p");
     /* 1B: Create a function:function customCreateElement(parent){ //body } */
 
-    function customCreateElement(parent) {
-        /* 1C:  In the body of customCreateElement create a new parargraph element*/
+    /* 1C:  In the body of customCreateElement create a new parargraph element*/
 
-        let newParagraph = document.createElement("p");
-        /* 1D:  Set the text of this element to be : `using create Element`*/
+    /* 1D:  Set the text of this element to be : `using create Element`*/
 
-        newParagraph.innerHTML = "<p> using create Element </p>";
+    /* 1E:  Set the background of this paragraph element to be green */
 
-        /* 1E:  Set the background of this paragraph element to be green */
 
-        newParagraph.style.background = "green";
+    /* 1F:  Set the color of the text in this paragraph element to be white */
 
-        /* 1F:  Set the color of the text in this paragraph element to be white */
 
-        newParagraph.style.color = "white";
-        /* 1G: Append this new element to the parent variable within the function. */
-
-        parent.append(newParagraph);
-    }
+    /* 1G: Append this new element to the parent variable within the function. */
 
 
     /* 1H: Iterate through the allPTagsThree array and call customCreateElement(),
     passing the current allPTagsThree element as the parent with each iteration.*/
     /***CODE */
+    let allPTagsThree = document.querySelectorAll("p");
+
+    function customCreateElement(parent) {
+
+        let newParagraph = document.createElement("p");
+
+        newParagraph.innerHTML = "<p> using create Element </p>";
+
+        newParagraph.style.background = "green";
+
+        newParagraph.style.color = "white";
+
+        parent.append(newParagraph);
+    }
     allPTagsThree.forEach((newP) => { customCreateElement(newP) });
 
     /***EXPLANATION::
@@ -254,24 +259,41 @@ length
     /*************************************** */
     /* 2: GRID OF BOXES */
     /* 2A: Create another new function: function customNewBoxCreate(parent){ //body }*/
-    /* 2B: In the body of customNewBoxCreate create a new div element, that has the class testDiv. 
-    /* 2C:Then append this new element to the parent variable within the function. 
+
+    /* 2B: In the body of customNewBoxCreate create a new div element, that has the class testDiv.
+    */
+    /* 2C:Then append this new element to the parent variable within the function.
+*/
     /* 2D:Finally, return</code> this new element */
-    /* 2E:Create a nested for loop (for rows and columns) to iterate through 10 columns and 10 rows (just like the JS Review :)). 
+
+    /* 2E:Create a nested for loop (for rows and columns) to iterate through 10 columns and 10 rows (just like the JS Review :)).
         Call the customNewBoxCreate function, in order to generate a new div -> representing each cell in the grid. 
         Ensure that the parent element for each of these new divs is the element whose id is named `new-grid`*/
-    /* 2F: You will see at this point that the x,y position of the resulting divs makes no sense... 
+
+    /* 2F: You will see at this point that the x,y position of the resulting divs makes no sense...
         Fix this by doing the following: every time you call customNewBoxCreate() - save the current returned element 
         in a variable i.e. returnedDiv. 
         Set the style (left and top) to the of this element to 
         the necessary x and y position (use the counter variables in the for nested for loop to 
         calculate the new positions.
+        
     /* 2G: BONUS I: Make every div in the resulting grid in an even numbered row have white background 
         and otherwise let it have a background of purple.</li>
     /* 2H: BONUS II: For every div in an even numbered row make it contain the text `EVEN`, 
         otherwise lat it have the content `ODD`.*/
 
     /***CODE */
+    function customNewBoxCreate(parent) {
+
+        let newDiv = document.createElement("div");
+
+        newDiv.classList.add("testDiv");
+
+        parent.append(newDiv);
+
+        return newDiv
+
+    };
 
 
     /***EXPLANATION::
