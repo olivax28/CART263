97459b6ddc5b45a4e9694802e17090dee73080ba
olivax28 <olivax28@outlook.com>
2025-02-11 14:35:52 -0500

    added cararel's code to the 3rd exercise

--- Week 04/CART263-TASK-3 -START-B/task-3-js/team-d.js
+++ Week 04/CART263-TASK-3 -START-B/task-3-js/team-d.js
@@ -71,10 +71,10 @@ function setup_D() {
       }
     } //for
   }
-   /**************** ANI A ************************************ */
+  /**************** ANI A ************************************ */
   /** PUT ALL YOUR CODE FOR ANIMATION A INSIDE  HERE */
   /**************** ANI A ************************************ */
-//new element
+  //new element
 
   /**************** TASK *******************************************
    * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
@@ -94,75 +94,76 @@ function setup_D() {
   function aniA(parentCanvas) {
     console.log("in A");
     let ballDiv = document.createElement("div");
-ballDiv.classList.add("TEAM_H_h_circle");
-ballDiv.style.left = "50px";
-ballDiv.style.top = "50px";
-ballDiv.style.width = "10px";
-ballDiv.style.height = "10px";
-ballDiv.setAttribute("groupD_boolean", "off");
+    ballDiv.classList.add("TEAM_H_h_circle");
+    ballDiv.style.left = "50px";
+    ballDiv.style.top = "50px";
+    ballDiv.style.width = "10px";
+    ballDiv.style.height = "10px";
+    ballDiv.setAttribute("groupD_boolean", "off");
 
 
 
-// access parent element
-parentCanvas.appendChild(ballDiv)
+    // access parent element
+    parentCanvas.appendChild(ballDiv)
 
 
 
 
 
-let speedX = 2;
-let speedY = 3;
-let bool = ballDiv.getAttribute("groupD_boolean");
+    let speedX = 2;
+    let speedY = 3;
+    let bool = ballDiv.getAttribute("groupD_boolean");
 
-  ballDiv.addEventListener ("click", function (){
-    bool = "on";
-    if (bool === "on"){
-      window.requestAnimationFrame(animate);
-      function animate() {   
-      ballDiv.style.left = parseInt(ballDiv.style.left) + speedX+ "px";
-      ballDiv.style.top = parseInt(ballDiv.style.top) + speedY + "px";
-      checkBounds(parentCanvas, ballDiv);
-      window.requestAnimationFrame(animate);
+    ballDiv.addEventListener("click", function () {
+      bool = "on";
+      if (bool === "on") {
+        window.requestAnimationFrame(animate);
+        function animate() {
+          ballDiv.style.left = parseInt(ballDiv.style.left) + speedX + "px";
+          ballDiv.style.top = parseInt(ballDiv.style.top) + speedY + "px";
+          checkBounds(parentCanvas, ballDiv);
+          window.requestAnimationFrame(animate);
 
-    }
-   
- 
- 
-}
- 
-// else if (event.code === "click") {
-//   let bool = document.getElementById("boxB").getAttribute("custom-bool");
-//   if (bool === "off") {
-//     document.getElementById("boxB").style.background = "orange";
-//     document.getElementById("boxB").setAttribute("custom-bool", "on");
-//   } else {
-//     ballDiv = "rgb(112, 184, 226)";
-//     document.getElementById("boxB").setAttribute("custom-bool", "off");
-//   }
-// }
-function checkBounds(parent, p) {
-  console.log(parseInt(p.style.left))
-  let bounds = parent.getBoundingClientRect();
-  console.log (bounds)
-  if (parseInt(p.style.left) > bounds.width) {
-    speedX*=-1;
- 
+        }
 
-  } 
-  else if (parseInt(p.style.left) < 0) {
-    speedX*=-1;
- 
+
+
+      }
+
+      // else if (event.code === "click") {
+      //   let bool = document.getElementById("boxB").getAttribute("custom-bool");
+      //   if (bool === "off") {
+      //     document.getElementById("boxB").style.background = "orange";
+      //     document.getElementById("boxB").setAttribute("custom-bool", "on");
+      //   } else {
+      //     ballDiv = "rgb(112, 184, 226)";
+      //     document.getElementById("boxB").setAttribute("custom-bool", "off");
+      //   }
+      // }
+      function checkBounds(parent, p) {
+        console.log(parseInt(p.style.left))
+        let bounds = parent.getBoundingClientRect();
+        console.log(bounds)
+        if (parseInt(p.style.left) > bounds.width) {
+          speedX *= -1;
+
+
+        }
+        else if (parseInt(p.style.left) < 0) {
+          speedX *= -1;
+
+        }
+
+        if (parseInt(p.style.top) > bounds.height) {
+          speedY *= -1;
+
+        }
+        else if (parseInt(p.style.top) < 0) {
+          speedY *= -1;
+        }
+      }
+    })
   }
- 
-  if (parseInt(p.style.top) > bounds.height) {
-    speedY*=-1;
- 
-  } 
-  else if (parseInt(p.style.top) < 0) {
-    speedY*=-1;
-  }
-   }
-  })}
 
   /**************** ANI B ************************************ */
   /** PUT ALL YOUR CODE FOR ANIMATION B INSIDE  HERE */
@@ -188,41 +189,84 @@ function checkBounds(parent, p) {
   /**************** ANI C ************************************ */
   /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN C INSIDE  HERE */
   /**************** ANI C ************************************ */
- /**************** TASK *******************************************
-   * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
-   * 1: use the PROVIDED keyup/down callbacks `windowKeyDownRef` and/or `windowKeyUpnRef` to handle keyboard events
-   * 2: create an interactive pattern/sketch based on keyboard input. Anything goes.
-   * do not use  requestAnimationFrame(), setInterval() nor setTimeout() -> meaning keep it simple ;)
-   * 
-   * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
-   * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
-   * this is so that your styles are not overriden by other teams.
-   * NOTE::: All your code is to be added here inside this function -
-   * remember you can define other functions inside....
-   * Do not change any code above or the HTML markup.
-   * **/
+  /**************** TASK *******************************************
+    * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
+    * 1: use the PROVIDED keyup/down callbacks `windowKeyDownRef` and/or `windowKeyUpnRef` to handle keyboard events
+    * 2: create an interactive pattern/sketch based on keyboard input. Anything goes.
+    * do not use  requestAnimationFrame(), setInterval() nor setTimeout() -> meaning keep it simple ;)
+    * 
+    * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
+    * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
+    * this is so that your styles are not overriden by other teams.
+    * NOTE::: All your code is to be added here inside this function -
+    * remember you can define other functions inside....
+    * Do not change any code above or the HTML markup.
+    * **/
 
 
   function aniC(parentCanvas) {
 
     console.log("in C");
-    /*** THIS IS THE CALLBACK FOR KEY DOWN ( DO NOT CHANGE THE NAME..) */
+    let blurAmount = 0;
+
+    //set background color  of canvas
+    console.log(parentCanvas.getBoundingClientRect());
+    // parentCanvas.style.backgroundColor = "rgb(175, 47, 83)";
+    let p = document.createElement("div");
+    p.classList.add("TEAM_D_d_circle");
+    parentCanvas.appendChild(p);
+    p.style.width = '10px';
+    p.style.height = '10px';
+    p.style.left = parentCanvas.getBoundingClientRect().width / 2 + "px";
+    p.style.top = parentCanvas.getBoundingClientRect().height / 2 + "px";
+    let speedX = 5;
+
+    /** THIS IS THE CALLBACK FOR KEY DOWN ( DO NOT CHANGE THE NAME ..)*/
     windowKeyDownRef = function (e) {
       //code for key down in here
-      console.log(e)
-      //SAMPLE KEY CHECK (you do not have to use)
-      if (e.code === "Space") {
-        console.log("team-space down")
-      }
-    };
+      console.log(e);
 
-    /*** THIS IS THE CALLBACK FOR KEY UP ( DO NOT CHANGE THE NAME..) */
-    windowKeyUpRef = function (e) {
-    //SAMPLE KEY CHECK (you do not have to use)
-      if (e.code === "Space") {
-        console.log("space up");
-        console.log("team-space up")
+      // if left key pressed
+      if (e.key === "ArrowLeft") {
+
+        // move ri
+        p.style.left = parseInt(p.style.left) + speedX + "px";
+
+        // if right key pressed
+      } else if (e.key === "ArrowRight") {
+
+        // move left
+        p.style.left = parseInt(p.style.left) - speedX + "px";
+
       }
+
+      // if left key pressed
+      if (e.key === "ArrowUp") {
+        if (parseInt(p.style.width) > 10) {
+          p.style.left = parseInt(p.style.left) - speedX + "px";
+          p.style.heileftght = parseInt(p.style.left) - speedX + "px";
+          parseInt(p.style.borderRadius) - speedX / 2 + "px";
+        }
+
+        // if right key
+      } else if (e.key === "ArrowDown") {
+        if (parseInt(p.style.width) < 250) {
+          p.style.width = parseInt(p.style.width) + speedX + "px";
+          p.style.height = parseInt(p.style.height) + speedX + "px";
+          parseInt(p.style.borderRadius) + speedX / 2 + "px";
+        }
+      }
+
+      //to white
+      else if (e.key === "-") {
+
+      }
+
+      //to blue
+      else if (e.key === "+") {
+
+      }
+
 
     };
 
