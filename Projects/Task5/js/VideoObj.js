c7490cbe6dfb56dde37d8ecc2c7356956ed90ce1
olivax28 <olivax28@outlook.com>
2025-03-11 17:18:17 -0400

    hue rotate now working

--- Projects/Task5/js/VideoObj.js
+++ Projects/Task5/js/VideoObj.js
@@ -7,18 +7,24 @@ class VideoObj {
     this.w = w;
     this.h = h;
     this.shapeX = 10;
-    this.shapeY =10;
+    this.shapeY = 10;
     this.shapeCol = "#000000";
- 
+
 
     let filterButton_blur = document.getElementById("filter_button_blur");
     let blurInput = document.getElementById("blurnum");
-    this.userProvidedBlur  = 0;
+    this.userProvidedBlur = 0;
     //added for sepia
     // replace blur with sepia
     let filterButton_sepia = document.getElementById("filter_button_sepia");
     let sepiaInput = document.getElementById("sepianum");
-    this.userProvidedSepia  = 0;
+    this.userProvidedSepia = 0;
+
+    let filterButton_hue = document.getElementById("filter_button_hue");
+    let hueInput = document.getElementById("huenum");
+    this.userProvidedHue = 0;
+
+
     let self = this;
 
     filterButton_blur.addEventListener("click", function () {
@@ -26,38 +32,49 @@ class VideoObj {
       self.userProvidedBlur = blurInput.value;
       console.log(self.userProvidedBlur);
 
-      
+
     });
+
     filterButton_sepia.addEventListener("click", function () {
       //get value from input field
-      self.userProvidedSepia= sepiaInput.value;
+      self.userProvidedSepia = sepiaInput.value;
       console.log(self.userProvidedSepia);
 
-      
+
     });
-   
+
+    filterButton_hue.addEventListener("click", function () {
+      //get value from input field
+      self.userProvidedHue = hueInput.value;
+      console.log(self.userProvidedHue);
+
+
+    });
+
 
   }
 
   display() {
     this.context.save();
-     this.context.filter = `blur(${this.userProvidedBlur}px)`;
-     //my filters
-     this.context.filter += `sepia(${this.userProvidedSepia*10}%)`;
+    this.context.filter = `blur(${this.userProvidedBlur}px)`;
+    //my filters
+    this.context.filter += `sepia(${this.userProvidedSepia * 10}%)`;
+
+    this.context.filter += `hue-rotate(${this.userProvidedHue}deg)`;
 
     this.context.drawImage(this.videoElement, this.x, this.y, this.w, this.h);
     this.context.fillStyle = this.shapeCol;
-    this.context.fillRect(this.shapeX, this.shapeY, 50,50)
+    this.context.fillRect(this.shapeX, this.shapeY, 50, 50)
     this.context.restore();
   }
 
-    //called when rectangle color is to be updated
-  changeColor(newCol){
-   /** FILL IN */
+  //called when rectangle color is to be updated
+  changeColor(newCol) {
+    /** FILL IN */
   }
   //called when rectangle Pos is to be updated
-  updatePositionRect(mx,my){
-     /** FILL IN */
+  updatePositionRect(mx, my) {
+    /** FILL IN */
   }
   update(videoElement) {
     this.videoElement = videoElement;
