fa486a64449bdab5af61ca77d6b78d676dd88c33
olivax28 <olivax28@outlook.com>
2025-04-09 22:07:47 -0400

    started task 7-8

--- Task7-8/CART263-TASK-7-8/js/main.js
+++ Task7-8/CART263-TASK-7-8/js/main.js
@@ -1,24 +1,60 @@
-window.onload = async function(){
-    console.log("task 7-8");
-        try {
-            console.log("in fetch");
-            let response = await fetch('files/iris.json');
-            let irises_original = await response.json();
-            console.log(irises_original);
-          //   document.querySelector("#resetButton").addEventListener("click", resetPoem);
-          //   document.querySelector("#stepOneButton").style.display = "none";
-          //   document.querySelector("#inputDiv").style.display = "block";
-          //   document.getElementById("rainbow_text").textContent = raw_rainbow_text;
-            
-            // console.log(raw_rainbow_text);
-      
-} catch (e) { console.log(e); }
-        
-    }
-    
+window.onload = async function () {
+  console.log("task 7-8");
+  try {
+
+    //Part 01
+    console.log("in fetch");
+    let response = await fetch('data/iris.json');
+    let irises_original = await response.json();
+    console.log(irises_original);
+
+
+
+
+    // Part 02
+
+
+    // let temp = getColorObj(el.color);
+    let possibleColor = ["#5d3fd3", "#a73fd3", "#d33fb5", "#d35d3f", "#d3a73f"]
+    const irisesWithColors = irises_original.map(iris => {
+      return {
+        ...iris,
+        color: possibleColor[Math.floor(Math.random() * possibleColor.length)]
+      };
+    });
+
+    console.log(irisesWithColors);
+
+    // Part 03
+
+    const filteredIrises = irisesWithColors.filter(iris => iris.sepalWidth < 4);
+
+    //Part 04
+
+
+  } catch (e) { console.log(e); }
 
 
 
 
 
-  
+  // Studen example:
+  // function changeColor() {
+  //   let possibleColor = ["#5d3fd3", "#a73fd3", "#d33fb5", "#d35d3f", "#d3a73f"]
+  //   const irisesWithColors = data.map(iris => {
+  //     return {
+  //       ...iris,
+  //       color: possibleColor[Math.floor(Math.random() * possibleColor.length)]
+  //     };
+  //   });
+
+  // }
+
+
+}
+
+
+
+
+
+
