4a0dffe573262cd3ba58c32291682957c2a0cd62
olivax28 <olivax28@outlook.com>
2025-04-15 18:01:16 -0400

    created the flower box for the irises

--- Task7-8/CART263-TASK-7-8/js/iris.js
+++ Task7-8/CART263-TASK-7-8/js/iris.js
@@ -1,24 +1,30 @@
 class iris {
-constructor (x,y,irisColor){
+    constructor(x, y, sepalLength, sepalWidth, petalLength, petalWidth, irisColor) {
 
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.irisColor = irisColor;
-    this.irisDiv = document.createElement("iris");
-  
-    
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.sepalLength = sepalLength;
+        this.sepalWidth = sepalWidth;
+        this.petalLength = petalLength;
+        this.petalWidth = petalWidth;
+        this.irisColor = irisColor;
+        this.irisPetalDiv = document.createElement("div");
+        this.irisSepalDiv = document.createElement("div");
 
+    }
 
-  
-    
-}
+    drawPetals() {
+        //structure taken from 'garden' project 
+        this.irisDiv.classList.add("iris");
+        this.irisDiv.style.width = this.petalWidth + "px";
+        this.irisDiv.style.height = this.petalLength + "px";
+        // this.irisDiv.style.background =  ;//will euqual to the given color. so a word, figure this out;
+        this.irisDiv.style.left = this.x + "px";
+        this.irisDiv.style.top = this.y + "px";
+        //add to the DOM
+        document.getElementsByClassName("flowerBox")[0].appendChild(this.irisDiv);
 
-
-renderIris() {
-    this.irisDiv.add("iris");
-    
-    document.getElementsByClassName("box")[0].appendChild(this.irisDiv);
-}
+    }
 
 }
--- Task7-8/CART263-TASK-7-8/js/main.js
+++ Task7-8/CART263-TASK-7-8/js/main.js
@@ -36,90 +36,107 @@ window.onload = async function () {
 
     //part 05
     const foundIris = irisesWithColors.find(
-      function(el){
-          return(el.petalWidth>1.0)
+      function (el) {
+        return (el.petalWidth > 1.0)
       })
-console.log(foundIris)
-//foundIris: {sepalLength: 7, sepalWidth: 3.2, petalLength: 4.7, petalWidth: 1.4, species: 'versicolor', …}
-// color
-// : 
-// "#5d3fd3"
-// petalLength
-// : 
-// 4.7
-// petalWidth
-// : 
-// 1.4
-// sepalLength
-// : 
-// 7
-// sepalWidth
-// : 
-// 3.2
-// species
-// : 
-// "versicolor"
-// [[Prototype]]
-// : 
-// Object
+    console.log(foundIris)
+    //foundIris: {sepalLength: 7, sepalWidth: 3.2, petalLength: 4.7, petalWidth: 1.4, species: 'versicolor', …}
+    // color
+    // : 
+    // "#5d3fd3"
+    // petalLength
+    // : 
+    // 4.7
+    // petalWidth
+    // : 
+    // 1.4
+    // sepalLength
+    // : 
+    // 7
+    // sepalWidth
+    // : 
+    // 3.2
+    // species
+    // : 
+    // "versicolor"
+    // [[Prototype]]
+    // : 
+    // Object
 
-//part 06
+    //part 06
 
-const hasPetalLengthLessthan10 = irisesWithColors.some(
-  function(el){
-      return (el.petalLength> 10)
-  }
-)
-console.log(hasPetalLengthLessthan10)
-//answer: False
+    const hasPetalLengthLessthan10 = irisesWithColors.some(
+      function (el) {
+        return (el.petalLength > 10)
+      }
+    )
+    console.log(hasPetalLengthLessthan10)
+    //answer: False
 
-//part07
-const petalEqualtoFour = irisesWithColors.some(
-  function(el){
-      return (el.petalLength == 4.2)
-  }
-)
-console.log(petalEqualtoFour)
-//answer: true
+    //part07
+    const petalEqualtoFour = irisesWithColors.some(
+      function (el) {
+        return (el.petalLength == 4.2)
+      }
+    )
+    console.log(petalEqualtoFour)
+    //answer: true
 
-//part 08
+    //part 08
 
-const everyPetalWidth3 = irisesWithColors .every(
-  function (el){
-      return(el.petalWidth < 3)
-  });
-console.log(everyPetalWidth3); 
-//answer: true
+    const everyPetalWidth3 = irisesWithColors.every(
+      function (el) {
+        return (el.petalWidth < 3)
+      });
+    console.log(everyPetalWidth3);
+    //answer: true
 
-//part 09
-const everySepalWidth = irisesWithColors .every(
-  function (el){
-      return(el.sepalWidth > 1.2)
-  });
-console.log(everySepalWidth); 
-//answer: true
+    //part 09
+    const everySepalWidth = irisesWithColors.every(
+      function (el) {
+        return (el.sepalWidth > 1.2)
+      });
+    console.log(everySepalWidth);
+    //answer: true
 
-//part 10
+    //part 10
 
 
-const irisesWithColorsSorted = irisesWithColors.toSorted((a, b) => a.petalWidth - b.petalWidth);
-console.log(irisesWithColorsSorted);
-//visualizing
-//IDEA: Bouquet of irises that grow bigger and smaller on their own with a for loop. and who's colors are the random assigned colors
+    const irisesWithColorsSorted = irisesWithColors.toSorted((a, b) => a.petalWidth - b.petalWidth);
+    console.log(irisesWithColorsSorted);
+    //visualizing
+    //IDEA: Bouquet of irises that grow bigger and smaller on their own with a for loop. and who's colors are the random assigned colors
 
 
 
 
   } catch (e) { console.log(e); }
+
+  //visualizing
+  //IDEA: Bouquet of irises that grow bigger and smaller on their own with a for loop. and who's colors are the random assigned colors
+
+  //Draw and add the FlowerBox to the DOM
+  let flowerBox = document.createElement("div");
+  flowerBox.classList.add("flowerBox");
+  // access parent element
+  let parentElement = document.querySelector("body")
+  parentElement.appendChild(flowerBox);
+
+  let rectangle = document.createElement("div");
+  rectangle.classList.add("rectangle");
+  let parentElementBox = document.querySelector(".flowerBox");
+  parentElementBox.appendChild(rectangle);
+
+
+
 }
 
 
 
 
-//visualizing
-//IDEA: Bouquet of irises that grow bigger and smaller on their own with a for loop. and who's colors are the random assigned colors
 
-document.createElement("box");
+
+
 
 
 
